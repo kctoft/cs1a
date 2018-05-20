fff34757efc79d6ac7a550b5dff41b6c6fdfb3da
Kitana Toft <kittoft@gmail.com>
2018-05-20 14:58:06 -0700

    Completed: array with random values

---
+++ assignments/arrayWithRandomValues/src/ArrayWithRandomValues.java
@@ -0,0 +1,36 @@
+/*
+* name: "Array with Random Values"
+* created: 5/19/2018
+* author: Kitana Toft
+* desc: Prints a number of randomly generated numbers based on user input
+ */
+
+import java.util.Scanner;
+import java.util.Random;
+import java.lang.Math;
+
+public class ArrayWithRandomValues {
+    public static void main(String [] args) {
+        // declare vars & initialize defaults
+        int arrayLength = 0;
+        Scanner s = new Scanner(System.in);
+        Random randGen = new Random();
+
+        // get user input & store it into arrayLength
+        System.out.println("Please enter a number: ");
+        arrayLength = s.nextInt();
+
+        // Allocate an array containing arrayLength integers
+        int[] randomArray = new int[arrayLength];
+
+        // and fill the array with random integers between 1 and 10.
+        for (int i = 0; i < arrayLength; i++) {
+            randomArray[i] = Math.abs(randGen.nextInt()) % 10 + 1;
+        }
+
+        // Finally print the array.
+        for (int i = 0; i < arrayLength; i++) {
+            System.out.println(randomArray[i]);
+        }
+    }
+}
