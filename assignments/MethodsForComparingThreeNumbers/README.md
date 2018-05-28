e762aed1de69937fdbe39fa6d64f3efb5ebce4b9
Kitana Toft <kittoft@gmail.com>
2018-05-28 13:16:29 -0700

    Added 5 methods that compare 3 numbers

---
+++ assignments/MethodsForComparingThreeNumbers/README.md
@@ -0,0 +1,32 @@
+# Methods For Comparing Three Numbers
+
+Define five new methods that each take three double values as parameters. 
+ Each method returns a result. None of these methods read from user input, 
+ and none of these methods prints to the console.
+ 
+ 1. smallest() 
+    * Takes 3 double values as parameters & returns the SMALLEST of the 3
+    * Sample: 
+    * System.out.println( smallest (3.5, 6.7, -8.5) );  # prints -8.5
+    
+ 2. largest()
+    * takes 3 double values as parameters & returns the LARGEST of the 3
+    * Sample:
+    * System.out.println( largest (3.5, 6.7, -8.5) );  # prints 6.7
+      
+ 3. average()
+    * takes 3 doubles & returns the AVERAGE of 3
+    * Sample:
+    * System.out.println( average (3.5, 6.7, -8.5) );  # prints  .57
+    
+ 4. allTheSame()
+    * method returns TRUE if all 3 doubles in parameter are of EQUAL value
+    * & returns FALSE if at least 1 value differs
+    * Sample:
+    * System.out.println( allTheSame (3.5, 6.7, -8.5) );  # prints  False
+ 
+ 5. allDifferent()
+    * method returns TRUE if all doubles sent in have DIFFERENT values
+    * returns FALSE if at least 2 of 3 are of EQUAL value
+    * Sample:
+    * System.out.println( allDifferent (3.5, 6.7, -8.5) );  # prints  True 
---
+++ assignments/MethodsForComparingThreeNumbers/src/MethodsForComparingThreeNumbers.java
@@ -0,0 +1,80 @@
+/*
+* name: Methods For Comparing Three Numbers
+* created on: 5/28/2018
+* author: Kitana Toft
+* desc: Contains five methods that compare three numbers in different ways
+ */
+public class MethodsForComparingThreeNumbers {
+    public static double smallest(double num1, double num2, double num3) {
+        // declare vars & defaults
+        double smallestVal = num1;
+
+        if (smallestVal > num2) {
+            smallestVal = num2;
+        }
+
+        if (smallestVal > num3) {
+            smallestVal = num3;
+        }
+
+        return smallestVal;
+    }
+
+    public static double largest(double num1, double num2, double num3) {
+        // declare vars & defaults
+        double largestVal = num1;
+
+        if (largestVal < num2) {
+            largestVal = num2;
+        }
+
+        if (largestVal < num3) {
+            largestVal = num3;
+        }
+
+        return largestVal;
+    }
+
+    public static double average(double num1, double num2, double num3) {
+        return (num1 + num2 + num3) / 3;
+    }
+
+    public static boolean allTheSame(double num1, double num2, double num3) {
+        if (num1 == num2 && num2 == num3) {
+            return true;
+        }
+        return false;
+    }
+
+    public static boolean allDifferent(double num1, double num2, double num3) {
+        if (num1 != num2 && num2 != num3 && num1 != num3) {
+            return true;
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        double num1 = 3.5;
+        double num2 = 6.7;
+        double num3 = -8.5;
+
+        System.out.println("smallest: "+ smallest(num1, num2, num3));
+        System.out.println("largest: " + largest(num1, num2, num3));
+        System.out.println("average: " + average(num1, num2, num3));
+        System.out.println("allTheSame: " + allTheSame(num1, num2, num3));
+        System.out.println("allDifferent: "+ allDifferent(num1, num2, num3));
+
+    }
+}
+
+/*
+Output:
+
+smallest: -8.5
+largest: 6.7
+average: 0.5666666666666664
+allTheSame: false
+allDifferent: true
+
+Process finished with exit code 0
+ */
