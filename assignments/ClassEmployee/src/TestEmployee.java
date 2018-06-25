caf8fd1b46a66d1fca4a745fad83ce916bbe28d0
Kitana Toft <kittoft@gmail.com>
2018-06-24 22:22:55 -0700

    asks for user input for Class Employee

--- assignments/ClassEmployee/src/TestEmployee.java
+++ assignments/ClassEmployee/src/TestEmployee.java
@@ -4,58 +4,28 @@
  * author: Kitana Toft
  * desc: Using methods from class Employee, test and print the solutions here.
  */
+import java.util.Scanner;
+import java.util.ArrayList;
 
 public class TestEmployee {
     public static void main(String[] args) {
-        Employee anEmployee = new Employee(); // initializes fields with the value of class definition
-        System.out.println(anEmployee);
+        Scanner s = new Scanner(System.in);
+        ArrayList<String> employeeList = new ArrayList<String>();
+        final int maxEmployees = 4;
 
-        // override defaults
-        anEmployee.setFirstName("Daniel");
-        System.out.println(anEmployee.getFirstName());
-        anEmployee.setLastName("Smith");
-        System.out.println(anEmployee.getLastName());
-        anEmployee.setSsn(100201234);
-        System.out.println(anEmployee.getSsn());
-        anEmployee.setSalary(90000);
-        System.out.println(anEmployee.getSalary());
+        System.out.println("Enter your employee's information below.");
+
+        System.out.println("Enter first name: ");
+        String name = s.next();
+        System.out.println("Enter last name: ");
+        String surName = s.next();
+        System.out.println("Enter social security number: ");
+        int ssn = s.nextInt();
+        System.out.println("Enter employee's salary: ");
+        float salary = s.nextFloat();
+
+        Employee anEmployee = new Employee(name, surName, ssn, salary);
 
        System.out.println(anEmployee);
-
-       Employee theEmployee = new Employee("Olivia", "Greenbottom", 987654321, 30000);
-
-       System.out.println(theEmployee);
-
-       // give Oliva Greenbottom a raise
-        theEmployee.giveRaise(23);
-        System.out.println(theEmployee);
     }
 }
-
-/**
- * Output:
-
- First name: Jane
- Last name: Doe
- Social security number: 123456789
- Salary: $100000.0
- Daniel
- Smith
- 100201234
- 90000.0
- First name: Daniel
- Last name: Smith
- Social security number: 100201234
- Salary: $90000.0
- First name: Olivia
- Last name: Greenbottom
- Social security number: 987654321
- Salary: $30000.0
- First name: Olivia
- Last name: Greenbottom
- Social security number: 987654321
- Salary: $36900.0
-
- Process finished with exit code 0
-
- */
