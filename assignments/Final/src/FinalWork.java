8c15178dc745717b72e0a62a8d45ec2403ecc7a4
Kitana Toft <kittoft@gmail.com>
2018-06-26 18:34:20 -0700

    question 2

--- assignments/Final/src/FinalWork.java
+++ assignments/Final/src/FinalWork.java
@@ -1,12 +1,18 @@
+import java.util.Scanner;
+
 public class FinalWork {
     public static void main(String[] args) {
-        int employeeDiscount;
-        employeeDiscount = 15;
-        double price;
-        price = 99.99;
-        double total;
-        total = price * ((100-(double)employeeDiscount)/100);
+        // declare & initialize scanner and age vars
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("What is your age?");
+        int age;
+        age = scanner.nextInt();
 
-        System.out.println(total);
+        // if/else statement that determines if user is a teenager
+        if (age < 13 || age > 19) {
+            System.out.println("You are not a teenager.");
+        } else {
+            System.out.println("You are a teenager.");
+        }
     }
 }
