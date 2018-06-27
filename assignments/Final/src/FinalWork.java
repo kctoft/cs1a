dc49c09eb5ab15c797b69dc338546ccd41ef78fb
Kitana Toft <kittoft@gmail.com>
2018-06-26 21:06:51 -0700

    question 8

--- assignments/Final/src/FinalWork.java
+++ assignments/Final/src/FinalWork.java
@@ -1,55 +1,29 @@
+/**
+ Assume that you are given a price in a String variable,
+ and you need to take a discount off of that price in order to calculate the total.
+ Recall that you cannot perform numeric calculations on String values.
+ You will need to convert the String value into a double value in order to perform arithmetic on it.
+ Luckily, there is a method in the Java API that converts a String value into a double value.
+ This method is called Double.parseDouble(), and it is defined inside class Double.
+ You do not have to import anything to call this method since class Double is inside the java.lang package.
+
+ In the following program, add the code that calls the method Double.parseDouble()
+ to convert the String priceInString into the double priceInDouble.
+ */
 
 public class FinalWork {
-    // declare variables
-    private int month;
-    private int day;
-    private int year;
-
-    // default constructor
-    public FinalWork() {
-        month = 6;
-        day = 26;
-        year = 2018;
-    }
-
-    public void setMonth(int month) {
-        month = month;
-    }
-
-    public int getMonth() {
-        return month;
-    }
-
-    public int setDay(int day) {
-        day = day;
-    }
-
-    public int getDay() {
-        return day;
-    }
-
-    public int setYear(int year) {
-        year = year;
-    }
-
-    public int getYear() {
-        return year;
-    }
-
-    public String today() {
-        String info = "";
-        info = month + "/" + day + "/" + year;
-        return info;
-    }
-}
-
     public static void main(String[] args) {
-        FinalWork today;
-        today = new FinalWork( );
-        today.setMonth(6);
-        today.setDay (25);
-        today.setYear(2018);
-        System.out.println(today);
+        final double DISCOUNT = .15;
+        String priceInString;
+        priceInString = "9.99";
+        double priceInDouble;
+
+        // this is where your code will go
+        priceInDouble = Double.parseDouble(priceInString);
+
+        double total;
+        total = priceInDouble - (priceInDouble * DISCOUNT);
+        System.out.println("The total including discount is: " + total);
     }
 }
 
