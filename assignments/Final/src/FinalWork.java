df555a2e85456e65ec441cb07196d722c31bd10a
Kitana Toft <kittoft@gmail.com>
2018-06-26 18:25:46 -0700

    question 1

---
+++ assignments/Final/src/FinalWork.java
@@ -0,0 +1,12 @@
+public class FinalWork {
+    public static void main(String[] args) {
+        int employeeDiscount;
+        employeeDiscount = 15;
+        double price;
+        price = 99.99;
+        double total;
+        total = price * ((100-(double)employeeDiscount)/100);
+
+        System.out.println(total);
+    }
+}
